8ea03b183279463f4328815bb8535fafc5235972
Yarin Orlev <yarin9191@gmail.com>
2025-04-17 18:27:46 +0300

    added swagger ui ep: /api

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // strips non-DTO fields
     forbidNonWhitelisted:true, // throws an error if extra fields exist
-  }));
+  })
+);
+  const config = new DocumentBuilder()
+  .setTitle('Thieves API')
+  .setDescription('API for managing thieves')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
